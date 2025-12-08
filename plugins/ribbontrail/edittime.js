8c18dfd6fe016617989bef0d3ad861c6348a371a
pigmalien <japigg@bellsouth.net>
2025-12-08 15:20:32 -0500

    feat: add RibbonTrail Construct 2 plugin for continuous ribbon mesh trails

---
+++ plugins/ribbontrail/edittime.js
@@ -0,0 +1,112 @@
+﻿function GetPluginSettings() {
+	return {
+		"name": "RibbonTrail",
+		"id": "RibbonTrail",
+		"version": "1.0",
+		"description": "A performant, continuous ribbon mesh trail plugin.",
+		"author": "Antigravity",
+		"help url": "https://www.construct.net/en/construct-2/manuals/construct-2-javascript-sdk",
+		"category": "General",
+		"type": "world",
+		"rotatable": true,
+		"flags": pf_texture | pf_position_aces | pf_size_aces | pf_angle_aces | pf_zorder_aces | pf_appearance_aces
+
+	};
+};
+
+////////////////////////////////////////
+// Parameter types:
+// AddNumberParam(label, description [, initial_string = "0"])
+// AddStringParam(label, description [, initial_string = "\"\""])
+// AddAnyTypeParam(label, description [, initial_string = "0"])
+// AddCmpParam(label, description)
+// AddComboParamOption(text)
+// AddComboParam(label, description [, initial_selection = 0])
+// AddObjectParam(label, description)
+// AddLayerParam(label, description)
+// AddLayoutParam(label, description)
+// AddKeybParam(label, description)
+// AddAnimationParam(label, description)
+// AddAudioFileParam(label, description)
+
+////////////////////////////////////////
+// Conditions
+
+////////////////////////////////////////
+// Actions
+
+AddNumberParam("X", "The X coordinate tracking position.");
+AddNumberParam("Y", "The Y coordinate tracking position.");
+AddAction(0, af_none, "Update Trail Position", "Ribbon", "Update trail position by ({0}, {1})", "Add a new tracking point to the ribbon trail.", "UpdateTrailPosition");
+
+////////////////////////////////////////
+// Expressions
+ACESDone();
+
+////////////////////////////////////////
+// Array of property grid properties for this plugin
+var property_list = [
+	new cr.Property(ept_float, "Ribbon Width", 40, "The maximum width of the ribbon mesh at its head (in pixels)."),
+	new cr.Property(ept_float, "Trail Lifespan", 0.5, "The total duration (in seconds) that a segment of the ribbon persists.")
+];
+
+// Called by IDE when a new object type is to be created
+function CreateIDEObjectType() {
+	return new IDEObjectType();
+}
+
+// Class representing an object type in the IDE
+function IDEObjectType() {
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+}
+
+// Called by IDE when a new object instance of this type is to be created
+IDEObjectType.prototype.CreateInstance = function (instance) {
+	return new IDEInstance(instance);
+}
+
+// Class representing an individual instance of an object in the IDE
+function IDEInstance(instance, type) {
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+
+	// Save the constructor parameters
+	this.instance = instance;
+	this.type = type;
+
+	// Set the default property values from the property table
+	this.properties = {};
+
+	for (var i = 0; i < property_list.length; i++)
+		this.properties[property_list[i].name] = property_list[i].initial_value;
+}
+
+// Called when inserted via Insert Object Dialog for the first time
+IDEInstance.prototype.OnInserted = function () {
+}
+
+// Called when double clicked in layout
+IDEInstance.prototype.OnDoubleClicked = function () {
+	this.instance.EditTexture();
+}
+
+
+// Called after a property has been changed in the properties bar
+IDEInstance.prototype.OnPropertyChanged = function (property_name) {
+}
+
+// For rendered objects to load fonts or textures
+IDEInstance.prototype.OnRendererInit = function (renderer) {
+}
+
+// Called to draw self in the editor if a layout object
+IDEInstance.prototype.Draw = function (renderer) {
+	// Draw a simple representation in the editor
+	renderer.SetTexture(this.instance.GetTexture());
+	var q = this.instance.GetBoundingQuad();
+	renderer.Quad(q, this.instance.GetOpacity());
+	renderer.Outline(q, cr.RGB(0, 0, 0));
+}
+
+// For rendered objects to release fonts or textures
+IDEInstance.prototype.OnRendererReleased = function (renderer) {
+}
---
+++ plugins/ribbontrail/runtime.js
@@ -0,0 +1,277 @@
+﻿// ECMAScript 5 strict mode
+"use strict";
+
+assert2(cr, "cr namespace not created");
+assert2(cr.plugins_, "cr.plugins_ not created");
+
+/////////////////////////////////////
+// Plugin class
+// *** CHANGE THE PLUGIN ID HERE *** - must match the "id" property in edittime.js
+//          vvvvvvvv
+cr.plugins_.RibbonTrail = function (runtime) {
+	this.runtime = runtime;
+};
+
+(function () {
+	/////////////////////////////////////
+	// *** CHANGE THE PLUGIN ID HERE *** - must match the "id" property in edittime.js
+	//                            vvvvvvvv
+	var pluginProto = cr.plugins_.RibbonTrail.prototype;
+
+	/////////////////////////////////////
+	// Object type class
+	pluginProto.Type = function (plugin) {
+		this.plugin = plugin;
+		this.runtime = plugin.runtime;
+	};
+
+	var typeProto = pluginProto.Type.prototype;
+
+	// called on startup for each object type
+	typeProto.onCreate = function () {
+		if (this.is_family) return;
+		this.texture_img = new Image();
+		this.texture_img.src = this.texture_file;
+	};
+
+	/////////////////////////////////////
+	// Instance class
+	pluginProto.Instance = function (type) {
+		this.type = type;
+		this.runtime = type.runtime;
+
+		// Initialize history array
+		this.pointHistory = [];
+	};
+
+	var instanceProto = pluginProto.Instance.prototype;
+
+	// called whenever an instance is created
+	instanceProto.onCreate = function () {
+		// Properties
+		this.ribbonWidth = this.properties[0];
+		this.trailLifespan = this.properties[1];
+		// Replaced textScrollSpeed with logic optimized static tiling
+
+		// Turn on ticking for garbage collection
+		this.runtime.tickMe(this);
+	};
+
+	instanceProto.onDestroy = function () {
+		this.pointHistory = [];
+	};
+
+	instanceProto.saveToJSON = function () {
+		return {
+			"ph": this.pointHistory
+		};
+	};
+
+	instanceProto.loadFromJSON = function (o) {
+		this.pointHistory = o["ph"] || [];
+	};
+
+	instanceProto.tick = function () {
+		var now = this.runtime.kahanTime.sum;
+		// Garbage Collection: remove old points
+		for (var i = this.pointHistory.length - 1; i >= 0; i--) {
+			if (now - this.pointHistory[i].time > this.trailLifespan) {
+				this.pointHistory.splice(i, 1);
+			} else {
+				break;
+			}
+		}
+
+		// Update Bounding Box to encompass the entire trail
+		if (this.pointHistory.length > 0) {
+			var minX = this.pointHistory[0].x;
+			var minY = this.pointHistory[0].y;
+			var maxX = minX;
+			var maxY = minY;
+
+			var r = this.ribbonWidth; // overestimate padding
+
+			for (var j = 1; j < this.pointHistory.length; j++) {
+				var p = this.pointHistory[j];
+				if (p.x < minX) minX = p.x;
+				if (p.x > maxX) maxX = p.x;
+				if (p.y < minY) minY = p.y;
+				if (p.y > maxY) maxY = p.y;
+			}
+
+			// Apply simple padding for width
+			this.bbox.set(minX - r, minY - r, maxX + r, maxY + r);
+			this.set_bbox_changed();
+		}
+
+		this.runtime.redraw = true;
+	};
+
+	instanceProto.draw = function (ctx) {
+		// 2D canvas fallback (optional, not requested, but good for stability)
+	};
+
+	instanceProto.drawGL = function (glw) {
+		// Use glw.quad processing.
+		var count = this.pointHistory.length;
+		if (count < 2) return;
+
+		// Setup texture
+		var tex = this.webGL_texture;
+
+		// Lazy-load type texture if instance has none
+		if (!tex) {
+			var type = this.type;
+			if (type.webGL_texture) {
+				tex = type.webGL_texture;
+			} else if (type.texture_img && type.texture_img.complete) {
+				// Load into WebGL
+				type.webGL_texture = this.runtime.glwrap.loadTexture(type.texture_img, false, this.runtime.linearSampling, this.type.texture_pixelformat);
+				tex = type.webGL_texture;
+			}
+		}
+
+		if (tex) {
+			glw.setTexture(tex);
+		} else {
+			// No texture warning (removed infinite spam)
+		}
+
+		var now = this.runtime.kahanTime.sum;
+		var points = this.pointHistory;
+
+		// OPTIMIZATION:
+		// Normals are now cached in pointHistory during UpdateTrailPosition.
+		// We only calculate W (Width) and Alpha here.
+
+		var objectOpacity = this.opacity;
+
+		// Loop through segments
+		for (var i = 0; i < count - 1; i++) {
+			var p1 = points[i];
+			var p2 = points[i + 1];
+
+			// Reconstruct geometry on the fly using cached normals
+			// p1.nx, p1.ny must exist.
+
+			// Fallback for missing normals (if old points exist before optimization)
+			// But user reloads, so clean start.
+
+			// 1. Calculate Widths and Alphas
+			var age1 = now - p1.time;
+			var lifeRatio1 = 1.0 - (age1 / this.trailLifespan);
+			if (lifeRatio1 < 0) lifeRatio1 = 0; if (lifeRatio1 > 1) lifeRatio1 = 1;
+
+			var w1 = this.ribbonWidth * lifeRatio1;
+			var hw1 = w1 * 0.5;
+
+			var age2 = now - p2.time;
+			var lifeRatio2 = 1.0 - (age2 / this.trailLifespan);
+			if (lifeRatio2 < 0) lifeRatio2 = 0; if (lifeRatio2 > 1) lifeRatio2 = 1;
+
+			var w2 = this.ribbonWidth * lifeRatio2;
+			var hw2 = w2 * 0.5;
+
+			var alpha = (lifeRatio1 + lifeRatio2) * 0.5 * objectOpacity;
+
+			// 2. Vertex positions
+			// P1
+			var l1x = p1.x + p1.nx * hw1;
+			var l1y = p1.y + p1.ny * hw1;
+			var r1x = p1.x - p1.nx * hw1;
+			var r1y = p1.y - p1.ny * hw1;
+
+			// P2
+			var l2x = p2.x + p2.nx * hw2;
+			var l2y = p2.y + p2.ny * hw2;
+			var r2x = p2.x - p2.nx * hw2;
+			var r2y = p2.y - p2.ny * hw2;
+
+			// 3. Draw Quad
+			// Order: TL, TR, BR, BL
+			glw.quad(l1x, l1y, r1x, r1y, r2x, r2y, l2x, l2y, alpha);
+		}
+	};
+
+	//////////////////////////////////////
+	// Conditions
+	function Cnds() { };
+	pluginProto.cnds = new Cnds();
+
+	//////////////////////////////////////
+	// Actions
+	function Acts() { };
+
+	Acts.prototype.UpdateTrailPosition = function (x, y) {
+		// Minimum distance check
+		if (this.pointHistory.length > 0) {
+			var last = this.pointHistory[0];
+			var dx = x - last.x;
+			var dy = y - last.y;
+			if (dx * dx + dy * dy < 2) return;
+		}
+
+		var now = this.runtime.kahanTime.sum;
+		if (isNaN(now)) now = 0;
+
+		// Create new point object
+		var newP = { x: x, y: y, time: now, nx: 0, ny: 0 };
+
+		// Add to history
+		this.pointHistory.unshift(newP);
+
+		// Calculate Normal for newP (Head) and Update previous Head
+		var count = this.pointHistory.length;
+		if (count >= 2) {
+			var p0 = this.pointHistory[0]; // New
+			var p1 = this.pointHistory[1]; // Old Head
+
+			// Calculate direction P0 -> P1
+			var dx = p1.x - p0.x;
+			var dy = p1.y - p0.y;
+			var len = Math.sqrt(dx * dx + dy * dy);
+			if (len > 0.001) { dx /= len; dy /= len; }
+			else { dx = 1; dy = 0; }
+
+			// Normal is (-dy, dx)
+			p0.nx = -dy;
+			p0.ny = dx;
+
+			// For P1 (Middle point now? Or just End?)
+			// If p1 was previously connecting to p2, we average it.
+			// If count == 2, p1 is Tail. Tail normal matches segment.
+			if (count > 2) {
+				var p2 = this.pointHistory[2];
+				// Vector P1 -> P2
+				var dx2 = p2.x - p1.x;
+				var dy2 = p2.y - p1.y;
+				var len2 = Math.sqrt(dx2 * dx2 + dy2 * dy2);
+				if (len2 > 0.001) { dx2 /= len2; dy2 /= len2; }
+
+				// Average direction (Angle bisector approximation)
+				var adx = (dx + dx2);
+				var ady = (dy + dy2);
+				var alen = Math.sqrt(adx * adx + ady * ady);
+				if (alen > 0.001) { adx /= alen; ady /= alen; }
+
+				// Normal
+				p1.nx = -ady;
+				p1.ny = adx;
+			} else {
+				// If only 2 points, P1 gets same normal as segment P0->P1
+				p1.nx = -dy;
+				p1.ny = dx;
+			}
+		}
+
+		this.runtime.redraw = true;
+	};
+
+	pluginProto.acts = new Acts();
+
+	//////////////////////////////////////
+	// Expressions
+	function Exps() { };
+	pluginProto.exps = new Exps();
+
+}());
