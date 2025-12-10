208e309ad7e36238b1c8c72d75aa5813392fdaee
pigmalien <japigg@bellsouth.net>
2025-12-10 15:06:19 -0500

    Add Tile-To Path behavior with runtime and editor support
    - Introduced new behavior "Tile-To Path" for moving sprites across a grid.
    - Added icon for the behavior.
    - Implemented common, edit-time, and runtime scripts for behavior functionality.
    - Defined parameters, conditions, actions, and expressions for the behavior.
    - Included debugging support for tracking movement state and path details.

---
+++ behaviors/tileto/edittime.js
@@ -0,0 +1,163 @@
+﻿function GetBehaviorSettings() {
+	return {
+		"name": "Tile-To Path",
+		"id": "TileToPath",
+		"version": "1.0",
+		"description": "Move a sprite across a grid, tile by tile.",
+		"author": "Antigravity",
+		"help url": "",
+		"category": "Movements",
+		"flags": 0
+	};
+};
+
+////////////////////////////////////////
+// Parameter types:
+// AddNumberParam(label, description [, initial_string = "0"])			// a number
+// AddStringParam(label, description [, initial_string = "\"\""])		// a string
+// AddAnyTypeParam(label, description [, initial_string = "0"])			// accepts either a number or string
+// AddCmpParam(label, description)										// combo with equal, not equal, less, etc.
+// AddComboParamOption(text)											// (repeat before "AddComboParam" to add combo items)
+// AddComboParam(label, description [, initial_selection = 0])			// a dropdown list parameter
+// AddObjectParam(label, description)									// a button to click and pick an object type
+// AddLayerParam(label, description)									// accepts either a layer number or name (string)
+// AddLayoutParam(label, description)									// a dropdown list with all project layouts
+// AddKeybParam(label, description)										// a button to click and press a key (returns a VK)
+// AddAudioFileParam(label, description)								// a dropdown list with all imported project audio files
+
+////////////////////////////////////////
+// Conditions
+
+// AddCondition(id,					// any positive integer to uniquely identify this condition
+//				flags,				// (see docs) cf_none, cf_trigger, cf_fake_trigger, cf_static, cf_not_invertible,
+//									// cf_deprecated, cf_incompatible_with_triggers, cf_looping
+//				list_name,			// appears in event wizard list
+//				category,			// category in event wizard list
+//				display_str,		// as appears in event sheet - use {0}, {1} for parameters and also <b></b>, <i></i>, and {my} for the current behavior icon & name
+//				description,		// appears in event wizard dialog when selected
+//				script_name);		// corresponding runtime function name
+
+// example				
+AddCondition(0, cf_trigger, "On path finished", "Movement", "On path finished", "Triggered when the object arrives at the final destination.", "OnPathFinished");
+
+AddCondition(1, cf_trigger, "On new tile reached", "Movement", "On new tile reached", "Triggered when the object arrives at a tile center.", "OnNewTileReached");
+
+AddCondition(2, cf_none, "Is moving", "Movement", "{my} is moving", "True if the object is currently moving.", "IsMoving");
+
+////////////////////////////////////////
+// Actions
+
+// AddAction(id,				// any positive integer to uniquely identify this action
+//			 flags,				// (see docs) af_none, af_deprecated
+//			 list_name,			// appears in event wizard list
+//			 category,			// category in event wizard list
+//			 display_str,		// as appears in event sheet - use {0}, {1} for parameters and also <b></b>, <i></i>
+//			 description,		// appears in event wizard dialog when selected
+//			 script_name);		// corresponding runtime function name
+
+// example
+AddNumberParam("Tile X", "The grid X coordinate to move to.");
+AddNumberParam("Tile Y", "The grid Y coordinate to move to.");
+AddAction(0, af_none, "Add tile to path", "Movement", "Add tile (<b>{0}</b>, <b>{1}</b>) to path", "Add a tile coordinate to the movement stack.", "AddTile");
+
+AddAction(1, af_none, "Clear path stack", "Movement", "Clear path stack", "Clear all pending move targets.", "ClearStack");
+
+AddAction(2, af_none, "Stop", "Movement", "Stop movement", "Stop moving immediately and clear the stack.", "StopMovement");
+
+AddAction(3, af_none, "Move along path", "Movement", "Move along path", "Start moving along the queued path.", "StartPath");
+
+////////////////////////////////////////
+// Expressions
+
+// AddExpression(id,			// any positive integer to uniquely identify this expression
+//				 flags,			// (see docs) ef_none, ef_deprecated, ef_return_number, ef_return_string,
+//								// ef_return_any, ef_variadic_parameters (one return flag must be specified)
+//				 list_name,		// currently ignored, but set as if appeared in event wizard
+//				 category,		// category in expressions panel
+//				 exp_name,		// the expression name after the dot, e.g. "foo" for "myobject.foo" - also the runtime function name
+//				 description);	// description in expressions panel
+
+// example
+AddExpression(0, ef_return_number, "Get stack count", "Movement", "StackCount", "Get the number of waypoints remaining in the stack.");
+
+AddExpression(1, ef_return_number, "Get target X", "Movement", "TargetX", "Get the current target grid X coordinate.");
+
+AddExpression(2, ef_return_number, "Get target Y", "Movement", "TargetY", "Get the current target grid Y coordinate.");
+
+AddNumberParam("WorldX", "The world X coordinate to convert.");
+AddExpression(3, ef_return_number, "World X to Tile X", "Conversion", "WorldToTileX", "Convert a world X coordinate to a grid column.");
+
+AddNumberParam("WorldY", "The world Y coordinate to convert.");
+AddExpression(4, ef_return_number, "World Y to Tile Y", "Conversion", "WorldToTileY", "Convert a world Y coordinate to a grid row.");
+
+AddNumberParam("TileX", "The grid X coordinate to convert.");
+AddExpression(5, ef_return_number, "Tile X to World X", "Conversion", "TileToWorldX", "Convert a grid column to a world X coordinate.");
+
+AddNumberParam("TileY", "The grid Y coordinate to convert.");
+AddExpression(6, ef_return_number, "Tile Y to World Y", "Conversion", "TileToWorldY", "Convert a grid row to a world Y coordinate.");
+
+AddExpression(7, ef_return_number, "Get current path index", "Movement", "CurrentIndex", "Get the 0-based index of the current waypoint in the path.");
+
+AddNumberParam("Index", "The 0-based index of the waypoint in the path.");
+AddExpression(8, ef_return_number, "Path X at index", "Movement", "PathXAtIndex", "Get the tile X coordinate for a given waypoint index in the current path.");
+
+AddNumberParam("Index", "The 0-based index of the waypoint in the path.");
+AddExpression(9, ef_return_number, "Path Y at index", "Movement", "PathYAtIndex", "Get the tile Y coordinate for a given waypoint index in the current path.");
+
+////////////////////////////////////////
+ACESDone();
+
+////////////////////////////////////////
+// Array of property grid properties for this plugin
+// new cr.Property(ept_integer,		name,	initial_value,	description)		// an integer value
+// new cr.Property(ept_float,		name,	initial_value,	description)		// a float value
+// new cr.Property(ept_text,		name,	initial_value,	description)		// a string
+// new cr.Property(ept_combo,		name,	"Item 1",		description, "Item 1|Item 2|Item 3")	// a dropdown list (initial_value is string of initially selected item)
+
+var property_list = [
+	new cr.Property(ept_integer, "Tile Width", 32, "Width of a grid tile in pixels."),
+	new cr.Property(ept_integer, "Tile Height", 32, "Height of a grid tile in pixels."),
+	new cr.Property(ept_float, "Speed", 100, "Movement speed in pixels per second."),
+	new cr.Property(ept_combo, "Allow Diagonals", "No", "Allow diagonal movement?", "No|Yes")
+];
+
+// Called by IDE when a new behavior type is to be created
+function CreateIDEBehaviorType() {
+	return new IDEBehaviorType();
+}
+
+// Class representing a behavior type in the IDE
+function IDEBehaviorType() {
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+}
+
+// Called by IDE when a new behavior instance of this type is to be created
+IDEBehaviorType.prototype.CreateInstance = function (instance) {
+	return new IDEInstance(instance, this);
+}
+
+// Class representing an individual instance of the behavior in the IDE
+function IDEInstance(instance, type) {
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+
+	// Save the constructor parameters
+	this.instance = instance;
+	this.type = type;
+
+	// Set the default property values from the property table
+	this.properties = {};
+
+	for (var i = 0; i < property_list.length; i++)
+		this.properties[property_list[i].name] = property_list[i].initial_value;
+
+	// any other properties here, e.g...
+	// this.myValue = 0;
+}
+
+// Called by the IDE after all initialization on this instance has been completed
+IDEInstance.prototype.OnCreate = function () {
+}
+
+// Called by the IDE after a property has been changed
+IDEInstance.prototype.OnPropertyChanged = function (property_name) {
+}
---
+++ behaviors/tileto/runtime.js
@@ -0,0 +1,312 @@
+﻿// ECMAScript 5 strict mode
+"use strict";
+
+assert2(cr, "cr namespace not created");
+assert2(cr.behaviors, "cr.behaviors not created");
+
+/////////////////////////////////////
+// Behavior class
+// *** CHANGE THE BEHAVIOR ID HERE *** - must match the "id" property in edittime.js
+//           vvvvvvvvvv
+cr.behaviors.TileToPath = function (runtime) {
+	this.runtime = runtime;
+};
+
+(function () {
+	// *** CHANGE THE BEHAVIOR ID HERE *** - must match the "id" property in edittime.js
+	//                               vvvvvvvvvv
+	var behaviorProto = cr.behaviors.TileToPath.prototype;
+
+	/////////////////////////////////////
+	// Behavior type class
+	behaviorProto.Type = function (behavior, objtype) {
+		this.behavior = behavior;
+		this.objtype = objtype;
+		this.runtime = behavior.runtime;
+	};
+
+	var behtypeProto = behaviorProto.Type.prototype;
+
+	behtypeProto.onCreate = function () {
+	};
+
+	/////////////////////////////////////
+	// Behavior instance class
+	behaviorProto.Instance = function (type, inst) {
+		this.type = type;
+		this.behavior = type.behavior;
+		this.inst = inst;				// associated object instance to modify
+		this.runtime = type.runtime;
+
+		this.stack = [];
+		this.currentMove = null;
+		this.subTarget = null;
+		this.isMoving = false;
+		this.pathTotalSize = 0;
+		this.originalPath = [];
+	};
+
+	var behinstProto = behaviorProto.Instance.prototype;
+
+	behinstProto.onCreate = function () {
+		// Load properties
+		this.tileW = this.properties[0];
+		this.tileH = this.properties[1];
+		this.speed = this.properties[2];
+		this.allowDiagonals = (this.properties[3] !== 0);
+	};
+
+	behinstProto.onDestroy = function () {
+	};
+
+	// called when saving the full state of the game
+	behinstProto.saveToJSON = function () {
+		return {
+			"stack": this.stack,
+			"currentMove": this.currentMove,
+			"subTarget": this.subTarget,
+			"isMoving": this.isMoving,
+			"isRunning": this.isRunning,
+			"pathTotalSize": this.pathTotalSize,
+			"originalPath": this.originalPath
+		};
+	};
+
+	// called when loading the full state of the game
+	behinstProto.loadFromJSON = function (o) {
+		this.stack = o["stack"] || [];
+		this.currentMove = o["currentMove"] || null;
+		this.subTarget = o["subTarget"] || null;
+		this.isMoving = o["isMoving"] || false;
+		this.isRunning = o["isRunning"] || false;
+		this.pathTotalSize = o["pathTotalSize"] || 0;
+		this.originalPath = o["originalPath"] || [];
+	};
+
+	behinstProto.tick = function () {
+		var dt = this.runtime.getDt(this.inst);
+
+		if (this.isRunning && !this.currentMove && this.stack.length > 0) {
+			this.currentMove = this.stack.shift();
+			this.isMoving = true;
+			this.subTarget = null;
+		}
+
+		if (this.currentMove) {
+			// If we don't have an immediate sub-target, calculate the next tile step
+			if (!this.subTarget) {
+				var cx = Math.floor(this.inst.x / this.tileW);
+				var cy = Math.floor(this.inst.y / this.tileH);
+
+				// Check if we reached the major target
+				if (cx === this.currentMove.tx && cy === this.currentMove.ty) {
+					// Ensure exact final snap
+					this.inst.x = this.currentMove.wx;
+					this.inst.y = this.currentMove.wy;
+					this.inst.set_bbox_changed();
+
+					this.currentMove = null;
+
+					if (this.stack.length === 0) {
+						this.isMoving = false;
+						this.isRunning = false;
+						this.runtime.trigger(cr.behaviors.TileToPath.prototype.cnds.OnPathFinished, this.inst);
+					}
+					return;
+				}
+
+				// Calculate direction to target
+				var dx = this.currentMove.tx - cx;
+				var dy = this.currentMove.ty - cy;
+				var nx = cx;
+				var ny = cy;
+
+				if (this.allowDiagonals) {
+					if (dx !== 0) nx += (dx > 0 ? 1 : -1);
+					if (dy !== 0) ny += (dy > 0 ? 1 : -1);
+				}
+				else {
+					// No diagonals: move along one axis at a time
+					if (dx !== 0) nx += (dx > 0 ? 1 : -1);
+					else if (dy !== 0) ny += (dy > 0 ? 1 : -1);
+				}
+
+				this.subTarget = {
+					wx: (nx * this.tileW) + (this.tileW / 2),
+					wy: (ny * this.tileH) + (this.tileH / 2),
+					tx: nx,
+					ty: ny
+				};
+			}
+
+			// Move towards subTarget
+			if (this.subTarget) {
+				var tx = this.subTarget.wx;
+				var ty = this.subTarget.wy;
+				var dx = tx - this.inst.x;
+				var dy = ty - this.inst.y;
+				var dist = Math.sqrt(dx * dx + dy * dy);
+				var step = this.speed * dt;
+
+				if (dist <= step) {
+					// Arrival at sub-target (tile center)
+					this.inst.x = tx;
+					this.inst.y = ty;
+					this.inst.set_bbox_changed();
+
+					this.runtime.trigger(cr.behaviors.TileToPath.prototype.cnds.OnNewTileReached, this.inst);
+
+					this.subTarget = null; // Forces calculation of next step in next frame
+				}
+				else {
+					// Move
+					// var angle = Math.atan2(dy, dx);
+					var angle = Math.atan2(dy, dx);
+					this.inst.x += Math.cos(angle) * step;
+					this.inst.y += Math.sin(angle) * step;
+					this.inst.set_bbox_changed();
+				}
+			}
+		}
+	};
+
+	/**BEGIN-PREVIEWONLY**/
+	behinstProto.getDebuggerValues = function (propsections) {
+		propsections.push({
+			"title": this.type.name,
+			"properties": [
+				{ "name": "Stack Count", "value": this.stack.length },
+				{ "name": "Is Moving", "value": this.isMoving },
+				{ "name": "Is Running", "value": this.isRunning },
+				{ "name": "Target X", "value": (this.currentMove ? this.currentMove.tx : 0) },
+				{ "name": "Target Y", "value": (this.currentMove ? this.currentMove.ty : 0) }
+			]
+		});
+	};
+
+	behinstProto.onDebugValueEdited = function (header, name, value) {
+	};
+	/**END-PREVIEWONLY**/
+
+	//////////////////////////////////////
+	// Conditions
+	function Cnds() { };
+
+	Cnds.prototype.OnPathFinished = function () {
+		return true;
+	};
+
+	Cnds.prototype.OnNewTileReached = function () {
+		return true;
+	};
+
+	Cnds.prototype.IsMoving = function () {
+		return this.isMoving;
+	};
+
+	behaviorProto.cnds = new Cnds();
+
+	//////////////////////////////////////
+	// Actions
+	function Acts() { };
+
+	Acts.prototype.AddTile = function (ix, iy) {
+		var wx = (ix * this.tileW) + (this.tileW / 2);
+		var wy = (iy * this.tileH) + (this.tileH / 2);
+
+		this.stack.push({
+			tx: ix,
+			ty: iy,
+			wx: wx,
+			wy: wy
+		});
+	};
+
+	Acts.prototype.ClearStack = function () {
+		this.stack.length = 0;
+		this.originalPath.length = 0;
+	};
+
+	Acts.prototype.StopMovement = function () {
+		this.stack.length = 0;
+		this.currentMove = null;
+		this.subTarget = null;
+		this.originalPath.length = 0;
+		this.isMoving = false;
+		this.isRunning = false;
+	};
+
+	Acts.prototype.StartPath = function () {
+		this.isRunning = true;
+		this.pathTotalSize = this.stack.length;
+		this.originalPath = this.stack.slice(0);
+	};
+
+	behaviorProto.acts = new Acts();
+
+	//////////////////////////////////////
+	// Expressions
+	function Exps() { };
+
+	Exps.prototype.StackCount = function (ret) {
+		ret.set_int(this.stack.length);
+	};
+
+	Exps.prototype.TargetX = function (ret) {
+		if (this.currentMove)
+			ret.set_int(this.currentMove.tx);
+		else
+			ret.set_int(-1);
+	};
+
+	Exps.prototype.TargetY = function (ret) {
+		if (this.currentMove)
+			ret.set_int(this.currentMove.ty);
+		else
+			ret.set_int(-1);
+	};
+
+	Exps.prototype.WorldToTileX = function (ret, x) {
+		ret.set_int(Math.floor(x / this.tileW));
+	};
+
+	Exps.prototype.WorldToTileY = function (ret, y) {
+		ret.set_int(Math.floor(y / this.tileH));
+	};
+
+	Exps.prototype.TileToWorldX = function (ret, tx) {
+		ret.set_int((tx * this.tileW) + (this.tileW / 2));
+	};
+
+	Exps.prototype.TileToWorldY = function (ret, ty) {
+		ret.set_int((ty * this.tileH) + (this.tileH / 2));
+	};
+
+	Exps.prototype.CurrentIndex = function (ret) {
+		if (this.currentMove)
+			ret.set_int(this.pathTotalSize - this.stack.length - 1);
+		else
+			ret.set_int(-1);
+	};
+
+	Exps.prototype.PathXAtIndex = function (ret, index) {
+		index = Math.floor(index);
+		if (index >= 0 && index < this.originalPath.length) {
+			ret.set_int(this.originalPath[index].tx);
+		} else {
+			ret.set_int(-1);
+		}
+	};
+
+	Exps.prototype.PathYAtIndex = function (ret, index) {
+		index = Math.floor(index);
+		if (index >= 0 && index < this.originalPath.length) {
+			ret.set_int(this.originalPath[index].ty);
+		} else {
+			ret.set_int(-1);
+		}
+	};
+
+	behaviorProto.exps = new Exps();
+
+}());
